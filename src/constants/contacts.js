b1f75faa50b9a9e3c85281e3a22684288f4df204
TaniaFed <ktv.poshta@gmail.com>
2025-05-03 23:26:01 -0400

    path

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,5 +1,7 @@
 import path from 'node:path';
 
-const pathToWorkDir = path.join(process.cwd());
+// export const PATH_DB = path.join( 'C:', 'Users', 'ktvpo', 'Documents', 'GitHub', 'Node.js', 'nodejs-hw-01', 'src', 'db', 'db.json');
 
+const pathToWorkDir = path.join(process.cwd());
 export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
+
